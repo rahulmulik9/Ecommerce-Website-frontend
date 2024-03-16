b9ddd56f62965eaa1288c2240f2627e57216796b
Rahul Mulik <rahulmulik9@gmail.com>
2024-03-16 18:31:07 +0530

    Navigation bar Carousel images autoplay with 2 sec infinity

---
+++ src/customer/components/HomeCarousel/MainCarousel.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { mainCarouselData } from './MainCarouselData';
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
+
+
+const MainCarousel = () => {
+    const items =mainCarouselData.map((item)=><img className='cursor-pointer' role='presentation'  src={item.image} alt=""/>)
+  return(  <AliceCarousel
+        items={items}
+        disableButtonsControls
+        autoPlay
+        autoPlayInterval={2000}
+        infinite
+    />
+    )
+}
+export  default MainCarousel;

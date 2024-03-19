bdf9653e58ce7ea2791e2bfb65043911a985c7f8
Rahul Mulik <rahulmulik9@gmail.com>
2024-03-19 21:05:40 +0530

    Home Product Sliding card finished

--- src/customer/components/HomeSectionCarousel/HomeSectionCarousel.jsx
+++ src/customer/components/HomeSectionCarousel/HomeSectionCarousel.jsx
@@ -1,27 +1,104 @@
 import React from "react";
-import HomeSectionCart from "../HomeSectionCart/HomeSectionCart";
-import AliceCarousel from 'react-alice-carousel';
-import 'react-alice-carousel/lib/alice-carousel.css';
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
+import { Button } from "@mui/material";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import { useState } from "react";
+import HomeProductCard from "./HomeProductCard";
 
-const HomeSectionCarousel = () => {
+const HomeSectionCarousel = ({section,data}) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const slidePrev = () => setActiveIndex(activeIndex - 1);
+  const slideNext = () => setActiveIndex(activeIndex + 1);
+  const syncActiveIndex = ({ item }) => setActiveIndex(item);
+
+
+  //as per the screen size the number of items will be shown 
   const responsive = {
-    0: { items: 1 },
-    568: { items: 3 },
-    1024: { items: 7 },
+    0: {
+      items: 2,
+      itemsFit: "contain",
+    },
+    568: {
+      items: 3,
+      itemsFit: "contain",
+    },
+    1024: {
+      items: 5.5,
+      itemsFit: "contain",
+    },
   };
-
-  const items = [11,1,1,1,1,11,5].map((items)=><HomeSectionCart/>)
-  return (
-    <div>
+  //data has been sent to child Homeproduct .ie. from home page
+  const items = data?.slice(0, 10).map((item) => (
+    <div className="">
       {" "}
-      <AliceCarousel
-        items={items}
-        disableButtonsControls
-        autoPlay
-        autoPlayInterval={1000}
-        infinite
-        responsive={responsive}
-      />
+      <HomeProductCard product={item} />
+    </div>
+  ));
+
+  // const slideInFromRight = (t) => {
+  //   return `translateX(${100 - t * 100}%)`;
+  // };
+
+  return (
+    <div className="relative px-4 sm:px-6 lg:px-8 ">
+      <h2 className="text-2xl font-extrabold text-gray-900 py-5">{section}</h2>
+      <div className="relative border p-5">
+        <AliceCarousel
+          disableButtonsControls
+          disableDotsControls
+          mouseTracking
+          items={items}
+          activeIndex={activeIndex}
+          responsive={responsive}
+          onSlideChanged={syncActiveIndex}
+          animationType="fadeout"
+          animationDuration={2000}
+        />
+        {/*if active index is eqaul to length the hide right hand  */}
+        {activeIndex !== items.length - 5 && (
+          <Button
+            onClick={slideNext}
+            variant="contained"
+            className="z-50 bg-[]"
+            sx={{
+              position: "absolute",
+              top: "8rem",
+              right: "0rem",
+              transform: "translateX(50%) rotate(90deg)",
+            }}
+            color="white"
+            aria-label="next"
+          >
+            <ArrowForwardIosIcon
+              className=""
+              sx={{ transform: "rotate(-90deg)" }}
+            />
+          </Button>
+        )}
+        {/*if active index is zero then left hand arrow button */}
+        {activeIndex !== 0 && (
+          <Button
+            onClick={slidePrev}
+            variant="contained"
+            className="z-50 bg-[]"
+            color="white"
+            sx={{
+              position: "absolute",
+              top: "8rem",
+              left: "0rem",
+              transform: "translateX(-50%)  rotate(90deg)",
+            }}
+            aria-label="next"
+          >
+            <ArrowForwardIosIcon
+              className=""
+              sx={{ transform: " rotate(90deg)" }}
+            />
+          </Button>
+        )}
+      </div>
     </div>
   );
 };
--- src/customer/pages/HomePage/HomePage.jsx
+++ src/customer/pages/HomePage/HomePage.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import MainCarousel from '../../components/HomeCarousel/MainCarousel'
 import HomeSectionCarousel from '../../components/HomeSectionCarousel/HomeSectionCarousel'
-
+import {mens_kurta} from '../../../Data/Men/men_kurta'
 const HomePage = () => {
   return (
     <div>
         <MainCarousel/>
         <div>
-            <HomeSectionCarousel></HomeSectionCarousel>
+            <HomeSectionCarousel data={mens_kurta} section={"Men's Kurta"}> </HomeSectionCarousel>
         </div>
     </div>
   )
